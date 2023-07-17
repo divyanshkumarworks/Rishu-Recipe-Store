9a8963b7ce58e2f304909540114f573d42d4bf8b
rishu <divyanshkumarworks@gmail.com>
2023-07-17 18:31:38 +0530

    fixed some issues

--- recipe_store/secrets.py
+++ recipe_store/secrets.py
@@ -1,3 +1,3 @@
-SECRET_KEY = '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'
+SECRET_KEY = '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'
 
 DJANGO_SECRET_KEY = 'django-insecure-n@ozlo*zr+((2uws29e=*6-n=u7gzm=n^7sx_t^6^$bwk_7xtk'
